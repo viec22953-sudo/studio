3dd48c1ba0234d633944b87bbe307c9306a0d38f
Jesse Ortiz <VIEC22953@gmail.com>
2025-10-31 18:11:13 +0000

    That didnt work

--- next.config.ts
+++ next.config.ts
@@ -28,12 +28,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'storage.cloud.google.com',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
