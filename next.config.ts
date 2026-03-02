55e52853b0ae989c62368942191e385e37af8374
Jesse Ortiz <jesseortiz@Jesses-Mac-mini.local>
2026-03-01 22:44:40 -0600

    fix: remove basePath to fix styling on custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/studio',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
